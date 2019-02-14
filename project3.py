996595b1f611b5c4b9f31cf0d6be529bbb748b1f
stephanievchheng <47198017+stephanievchheng@users.noreply.github.com>
2019-02-14 10:42:02 -0600

    Update project3.py

--- project3.py
+++ project3.py
@@ -2,23 +2,26 @@
 # from datetime import date
 # from datetime import time
 # from datetime import datetime
-# import urllib.request
+import urllib.request
 
-# f = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
+def main():
 
-# Open file for reading
-f = open("http_access_log.txt","r")
+    # File request from the internet
+    # f = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
 
-# inialize log request count 
-count = 0
-word = "GET"
+    # Open file for reading
+    f = open("http_access_log.txt","r")
 
-# Read lines in file
-searchlines = f.readlines()
-for x in searchlines:
-    if word in x.split():
-        count = count+1
-print("Total requests made in the time period of this log: " + str(count))
+    # inialize log request count 
+    count = 0
+    word = "GET"
 
-# Close file
-f.close()
+    # Read lines in file
+    searchlines = f.readlines()
+    for x in searchlines:
+        if word in x.split():
+            count = count+1
+    print("Total requests made in the time period of this log: " + str(count))
+
+    # Close file
+    f.close()
