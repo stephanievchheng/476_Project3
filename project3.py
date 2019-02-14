2ceac8c16f7ff8cc58d0023a1982593f78b0dc0b
stephanievchheng <47198017+stephanievchheng@users.noreply.github.com>
2019-02-14 10:26:19 -0600

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,24 @@
+# Python libraries 
+# from datetime import date
+# from datetime import time
+# from datetime import datetime
+# import urllib.request
+
+# f = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
+
+# Open file for reading
+f = open("http_access_log.txt","r")
+
+# inialize log request count 
+count = 0
+word = "GET"
+
+# Read lines in file
+searchlines = f.readlines()
+for x in searchlines:
+    if word in x.split():
+        count = count+1
+print("Total requests made in the time period of this log: " + str(count))
+
+# Close file
+f.close()
